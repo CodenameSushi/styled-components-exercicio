ae510311228abf6dfff271f7d123607cb237e9e1
CodenameSushi <fhenrique08@gmail.com>
2022-10-16 20:52:25 -0300

    Exercicios 034

--- src/Componentes/CardVideo.js
+++ src/Componentes/CardVideo.js
@@ -1,12 +1,14 @@
+import { StyledBoxVideo, StyledImg } from "./StyledCardVideo"
+
 function CardVideo(props) {
   function reproduzVideo(props) {
     alert("O vídeo está sendo reproduzido");
   }
   return (
-    <div className="box-pagina-principal" onClick={reproduzVideo}>
-      <img src={props.image1} alt={props.textoAlternativo} />
+    <StyledBoxVideo onClick={reproduzVideo}>
+      <StyledImg src={props.image1} alt={props.textoAlternativo} />
       <h4>{props.titulo}</h4>
-    </div>
+    </StyledBoxVideo>
   );
 }
 
